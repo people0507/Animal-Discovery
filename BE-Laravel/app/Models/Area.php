ee5bfa9857a923e0c591fe4f1bbeba817e216013
Nguyễn Đăng Nhân <2051063581@e.tlu.edu.vn>
2024-06-04 00:06:25 +0700

    update model and relationship

---
+++ BE-Laravel/app/Models/Area.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Area extends Model
+{
+    use HasFactory;
+    protected $table ='animal_area';
+
+    public function animalDetail()
+    {
+        return $this->belongsToMany(AnimalDetail::class,'animal_between_area','animal_detail_id', 'animal_area_id');
+    }
+}
